27cd069bd5d6d10781efc5e7875ebc1b1a78b110
Mikael thepault <mikael.thepault@pasapas.com>
2023-06-14 09:00:32 +0000

    amélioration appel EndPoint

--- calculequiui/webapp/ext/controller/SendAgoraHandler.js
+++ calculequiui/webapp/ext/controller/SendAgoraHandler.js
@@ -12,12 +12,12 @@ sap.ui.define([
             ajaxRequest.method = "POST";
             ajaxRequest.headers = { "Content-Type": "application/json"};
             jQuery.ajax(ajaxRequest);
-
-
             */
-
+            var sServiceUrl = this.editFlow.getView().getModel().aBindings[0].oModel.sServiceUrl;
+            var url = sServiceUrl.replace("/catalog/", "/iflowagora/http/newijt/post");
             $.ajax ({
-            url: "./iflowagora/http/newijt/post", 
+            //url: "./iflowagora/http/newijt/post", // Fonctionne avec BAS et déploiement local
+            url: url,
             type: "POST", 
             data: "",
             headers: {
